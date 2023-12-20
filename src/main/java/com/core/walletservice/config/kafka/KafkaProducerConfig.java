ae0889571b5b9940da96609dee27f6839c3b1bec
LeoSigmao <Leo.c@sigmaonetechnologies.com>
2023-12-20 22:13:13 +0200

    refactoring changes and some issue fixes for deposit and withdraw API

---
+++ src/main/java/com/core/walletservice/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,37 @@
+package com.core.walletservice.config.kafka;
+
+import com.core.walletservice.dto.TransactionDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    public KafkaProducerConfig() {
+    }
+
+    @Bean
+    public ProducerFactory<String, TransactionDTO> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, TransactionDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- src/main/java/com/core/walletservice/repositories/WalletRepository.java
+++ src/main/java/com/core/walletservice/repositories/WalletRepository.java
@@ -3,11 +3,18 @@ package com.core.walletservice.repositories;
 import com.core.walletservice.entity.Wallet;
 import com.core.walletservice.exceptions.EntityNotFoundException;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface WalletRepository extends MongoRepository<Wallet, UUID>, CustomWalletRepository {
 
     Wallet findByUsername(String username) throws EntityNotFoundException;
+
+    @Query("""
+{"username": {$in: ?0}}""")
+    List< Wallet> findAllByUpline(List<String> usernameIds) ;
+
 
 }
