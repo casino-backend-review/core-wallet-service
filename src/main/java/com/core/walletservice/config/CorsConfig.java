d742ee63576785afee92f9650a9858f63ffadc61
ShreeRg <shree@sigmaonetechnologies.com>
2024-02-17 14:54:31 +0530

    Updated crossOrigin configuration

---
+++ src/main/java/com/core/walletservice/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.core.walletservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000", "http://your-ui-domain:port")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
